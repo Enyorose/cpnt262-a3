ba1c60c46dd3ad339a5e8af0c2fc524df3a0452b
Jessica.W <jessica.watson1@live.com>
2021-10-26 09:12:05 -0600

    moved code into my fetch function

--- js/ghibli-api.js
+++ js/ghibli-api.js
@@ -1,4 +1,4 @@
-fetch('https://ghibliapi.herokuapp.com/films/2baf70d1-42bb-4437-b551-e5fed5a87abe')
+fetch('https://ghibliapi.herokuapp.com/films')
   .then(function(response){
     // JSON that is returned from the server must be converted to a JS object asynchronously.
     if (!response.ok) {
@@ -7,21 +7,22 @@ fetch('https://ghibliapi.herokuapp.com/films/2baf70d1-42bb-4437-b551-e5fed5a87ab
     return response.json();
   })
   .then(function(data){
-   
+    const movie = document.querySelector(".movies")
+
+  movie.innerHTML = `
+  <figure>
+  <img src="${data.data.image}" alt="${data.title}">
+  <figcaption>${data.description}</figcaption>
+  </figure>
+  ` 
     // Any code that depends on the `data` must go in this block
     console.log(data);
   })
   .catch(function(err){
     // An error or `reject` from any of the above `.then()` blocks will end up here.
+    
     console.log(err);
   });
 
-  const movie = document.querySelector(".movies")
-
-  movie.innerHTML = `
-  <figure>
-  <img src="${data.image}" alt="${data.title}">
-  <figcaption>${data.description}</figcaption>
-  </figure>
-  `  
+  
 
